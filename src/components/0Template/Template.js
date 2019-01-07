a6eca2190d20343aa6a0e96e756f4569d2c016b4
Rod Glover <rglover@uvic.ca>
2019-01-07 14:47:40 -0800

    Simplify component template

--- src/components/0Template/Template.js
+++ src/components/0Template/Template.js
@@ -1,21 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import _ from 'underscore';
-
-import './Template.css';
-
 
 export default class Template extends React.Component {
   static propTypes = {
   };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    };
-  }
+  state = {
+  };
 
   render() {
     return (
