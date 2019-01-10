31c5bcad9345785d10a96aefa8847ca471f6e4d4
Rod Glover <rglover@uvic.ca>
2019-01-10 15:34:03 -0800

    Add sketch nav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import SketchA from '../sketches/SketchA';
+
+const navSpec = [
+  { label: 'Sketch A', path: 'SketchA', component: SketchA },
+];
 
 
 export default class Template extends React.Component {
@@ -12,7 +21,31 @@ export default class Template extends React.Component {
 
   render() {
     return (
-      <SketchA/>
+      <Router basename={'/#'}>
+        <div>
+          <Navbar fluid>
+            <Nav>
+              {
+                navSpec.map(({label, path}) => (
+                  <LinkContainer to={`/${path}`}>
+                    <NavItem eventKey={path}>
+                      {label}
+                    </NavItem>
+                  </LinkContainer>
+                ))
+              }
+            </Nav>
+          </Navbar>
+
+          <Switch>
+            {
+              navSpec.map(({path, component}) => (
+                <Route path={`/${path}`} component={component}/>
+              ))
+            }
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
