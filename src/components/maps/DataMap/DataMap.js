debdf8fa1376eb2109c238dfdb3c7222b9048a5d
Rod Glover <rglover@uvic.ca>
2019-01-10 16:59:25 -0800

    Add non-content data map

---
+++ src/components/maps/DataMap/DataMap.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import BCBaseMap from '../BCBaseMap';
+import './DataMap.css'
+
+
+export default class DataMap extends React.Component {
+  static propTypes = {
+  };
+
+  state = {
+  };
+
+  render() {
+    return (
+      <BCBaseMap/>
+    );
+  }
+}
