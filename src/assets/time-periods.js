bce7d0c79764323a9ad9b3ae830c315336ff6bd0
Rod Glover <rglover@uvic.ca>
2019-01-21 12:14:38 -0800

    Update timePeriods asset

--- src/assets/time-periods.js
+++ src/assets/time-periods.js
@@ -1,14 +1,23 @@
 export default [
   {
     label: '2020s (2010-2039)',
-    value: '2020',
+    value: {
+      start_date: 2010,
+      end_date: 2039,
+    },
   },
   {
     label: '2050s (2040-2069)',
-    value: '2050',
+    value: {
+      start_date: 2040,
+      end_date: 2069,
+    },
   },
   {
     label: '2080s (2070-2099)',
-    value: '2080',
+    value: {
+      start_date: 2070,
+      end_date: 2099,
+    },
   },
 ];
