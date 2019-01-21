69dedf807cb0d95bbabd61b5cdfe4fc61b69275f
Rod Glover <rglover@uvic.ca>
2019-01-21 12:18:41 -0800

    Refactor TwoDataMaps props

--- src/components/maps/TwoDataMaps/TwoDataMaps.js
+++ src/components/maps/TwoDataMaps/TwoDataMaps.js
@@ -3,10 +3,16 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import DataMap from '../../maps/DataMap';
 import BCBaseMap from '../BCBaseMap';
+import seasons from '../../../assets/seasons';
 
 
 export default class TwoDataMaps extends React.Component {
   static propTypes = {
+    region: PropTypes.object,
+    historicalTimePeriod: PropTypes.object,
+    futureTimePeriod: PropTypes.object,
+    season: PropTypes.object,
+    variable: PropTypes.object,
   };
 
   state = {
@@ -22,17 +28,33 @@ export default class TwoDataMaps extends React.Component {
     return (
       <Row>
         <Col lg={6}>
-          <h3>Historical: 1961-1990</h3>
+          <h3>{`
+            Historical:
+            ${this.props.historicalTimePeriod.start_date}-
+            ${this.props.historicalTimePeriod.end_date}
+            `}</h3>
           <DataMap
             viewport={this.state.viewport}
             onViewportChange={this.handleChangeViewport}
+            region={this.props.region}
+            season={this.props.season}
+            variable={this.props.variable}
+            timePeriod={this.props.historicalTimePeriod}
           />
         </Col>
         <Col lg={6}>
-          <h3>Projected: {this.props.timePeriod.label}</h3>
+          <h3>{`
+            Projected:
+            ${this.props.futureTimePeriod.start_date}-
+            ${this.props.futureTimePeriod.end_date}
+          `}</h3>
           <DataMap
             viewport={this.state.viewport}
             onViewportChange={this.handleChangeViewport}
+            region={this.props.region}
+            season={this.props.season}
+            variable={this.props.variable}
+            timePeriod={this.props.futureTimePeriod}
           />
         </Col>
       </Row>
