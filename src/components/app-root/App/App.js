5e3e443bc0c3406c2be5f9235ebd0d73aae3b785
Rod Glover <rglover@uvic.ca>
2019-04-03 14:32:10 -0700

    Redo app layout with RB 1.0

--- src/components/app-root/App/App.js
+++ src/components/app-root/App/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import {
-  Grid, Row, Col, Tabs, Tab, Panel,
-} from 'react-bootstrap';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Card from 'react-bootstrap/Card';
+
 import { filter } from 'lodash/fp';
+
 import regions from '../../../assets/regions';
 import timePeriods from '../../../assets/time-periods';
 import seasons from '../../../assets/seasons';
 import variables from '../../../assets/variables';
 import meta from '../../../assets/meta';
+
 import AppHeader from '../AppHeader';
 import RegionSelector from '../../selectors/RegionSelector/RegionSelector';
 import TimePeriodSelector from '../../selectors/TimePeriodSelector/TimePeriodSelector';
@@ -38,136 +45,124 @@ export default class App extends Component {
 
   render() {
     return (
-      <Grid fluid>
+      <Container fluid>
         <AppHeader/>
 
         <Row>
           <Col lg={2}>
-            <Panel>
-              <Panel.Body>
-                <SelectorLabel>I am interested in information about projected climate change ...</SelectorLabel>
-                <SelectorLabel>... for the region of</SelectorLabel>
-                <RegionSelector
-                  value={this.state.region}
-                  onChange={this.handleChangeRegion}
-                />
+            <SelectorLabel>I am interested in information about projected climate change ...</SelectorLabel>
+            <SelectorLabel>... for the region of</SelectorLabel>
+            <RegionSelector
+              value={this.state.region}
+              onChange={this.handleChangeRegion}
+            />
 
-                <SelectorLabel>... in the future time period</SelectorLabel>
-                <TimePeriodSelector
-                  bases={filter(m => +m.start_date >= 2010)(meta)}
-                  value={this.state.futureTimePeriod}
-                  onChange={this.handleChangeTimePeriod}
-                />
+            <SelectorLabel>... in the future time period</SelectorLabel>
+            <TimePeriodSelector
+              bases={filter(m => +m.start_date >= 2010)(meta)}
+              value={this.state.futureTimePeriod}
+              onChange={this.handleChangeTimePeriod}
+            />
 
-                <SelectorLabel>... showing a typical (average)</SelectorLabel>
-                <SeasonSelector
-                  value={this.state.season}
-                  onChange={this.handleChangeSeason}
-                />
+            <SelectorLabel>... showing a typical (average)</SelectorLabel>
+            <SeasonSelector
+              value={this.state.season}
+              onChange={this.handleChangeSeason}
+            />
 
-                <SelectorLabel>for that period.</SelectorLabel>
-              </Panel.Body>
-            </Panel>
+            <SelectorLabel>for that period.</SelectorLabel>
           </Col>
 
           <Col lg={10}>
-            <Panel>
-              <Panel.Body>
-                <Tabs
-                  id={'main'}
-                  defaultActiveKey={'Maps'}
-                >
-                  <Tab eventKey={'Summary'} title={'Summary'}>
-                    Summary Content
-                  </Tab>
+            <Tabs
+              id={'main'}
+              defaultActiveKey={'Maps'}
+            >
+              <Tab eventKey={'Summary'} title={'Summary'}>
+                Summary Content
+              </Tab>
 
-                  <Tab eventKey={'Impacts'} title={'Impacts'}>
-                    <Row>
-                      <Col lg={12}>
-                        <Panel>
-                          <Panel.Body>
-                            <Tabs
-                              id={'impacts'}
-                              defaultActiveKey={'by-impact'}
-                            >
-                              <Tab eventKey={'by-impact'} title={'By Impact'}>
-                                <ImpactsByImpact/>
-                              </Tab>
-                              <Tab eventKey={'by-sector'} title={'By Sector'}>
-                                <ImpactsBySector/>
-                              </Tab>
-                            </Tabs>
-                          </Panel.Body>
-                        </Panel>
-                      </Col>
-                    </Row>
-                  </Tab>
+              <Tab eventKey={'Impacts'} title={'Impacts'}>
+                <Row>
+                  <Col lg={12}>
+                    <Tabs
+                      id={'impacts'}
+                      defaultActiveKey={'by-impact'}
+                    >
+                      <Tab eventKey={'by-impact'} title={'By Impact'}>
+                        <ImpactsByImpact/>
+                      </Tab>
+                      <Tab eventKey={'by-sector'} title={'By Sector'}>
+                        <ImpactsBySector/>
+                      </Tab>
+                    </Tabs>
+                  </Col>
+                </Row>
+              </Tab>
 
-                  <Tab eventKey={'Maps'} title={`Maps`}>
-                    <Row>
-                      <Col lg={2}>
-                        <SelectorLabel>Show details about</SelectorLabel>
-                        <VariableSelector
-                          bases={meta}
-                          value={this.state.variable}
-                          onChange={this.handleChangeVariable}
-                        />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12}>
-                        <h2>{`
-                          ${this.state.season.label}
-                          ${this.state.variable.label}
-                          for the ${this.state.region.label} region
-                        `}</h2>
-                      </Col>
-                    </Row>
-                    <TwoDataMaps
-                      region={this.state.region.value}
-                      historicalTimePeriod={{
-                        start_date: 1961,
-                        end_date: 1990,
-                      }}
-                      futureTimePeriod={this.state.futureTimePeriod.value}
-                      season={this.state.season.value}
-                      variable={this.state.variable.value}
+              <Tab eventKey={'Maps'} title={`Maps`}>
+                <Row>
+                  <Col lg={2}>
+                    <SelectorLabel>Show details about</SelectorLabel>
+                    <VariableSelector
+                      bases={meta}
+                      value={this.state.variable}
+                      onChange={this.handleChangeVariable}
                     />
-                  </Tab>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg={12}>
+                    <h2>{`
+                      ${this.state.season.label}
+                      ${this.state.variable.label}
+                      for the ${this.state.region.label} region
+                    `}</h2>
+                  </Col>
+                </Row>
+                <TwoDataMaps
+                  region={this.state.region.value}
+                  historicalTimePeriod={{
+                    start_date: 1961,
+                    end_date: 1990,
+                  }}
+                  futureTimePeriod={this.state.futureTimePeriod.value}
+                  season={this.state.season.value}
+                  variable={this.state.variable.value}
+                />
+              </Tab>
 
-                  <Tab eventKey={'Graph'} title={`Graph`}>
-                    <Row>
-                      <Col lg={2}>
-                        <SelectorLabel>Show details about</SelectorLabel>
-                        <VariableSelector
-                          bases={meta}
-                          value={this.state.variable}
-                          onChange={this.handleChangeVariable}
-                        />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12}>
-                        <h2>{`
-                          Range of projected change in
-                          ${this.state.season.label}
-                          ${this.state.variable.label}
-                          for the ${this.state.region.label} region
-                        `}</h2>
-                      </Col>
-                      <Col lg={6}>
-                        <ChangeOverTimeGraph
-                          {...this.state}
-                        />
-                      </Col>
-                    </Row>
-                  </Tab>
-                </Tabs>
-              </Panel.Body>
-            </Panel>
+              <Tab eventKey={'Graph'} title={`Graph`}>
+                <Row>
+                  <Col lg={2}>
+                    <SelectorLabel>Show details about</SelectorLabel>
+                    <VariableSelector
+                      bases={meta}
+                      value={this.state.variable}
+                      onChange={this.handleChangeVariable}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg={12}>
+                    <h2>{`
+                      Range of projected change in
+                      ${this.state.season.label}
+                      ${this.state.variable.label}
+                      for the ${this.state.region.label} region
+                    `}</h2>
+                  </Col>
+                  <Col lg={6}>
+                    <ChangeOverTimeGraph
+                      {...this.state}
+                    />
+                  </Col>
+                </Row>
+              </Tab>
+            </Tabs>
           </Col>
         </Row>
-      </Grid>
+      </Container>
     );
   }
 }
