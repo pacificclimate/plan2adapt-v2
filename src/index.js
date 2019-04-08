339ccebdd46bb4a7fa9f58dc3505830bce6c4c78
Rod Glover <rglover@uvic.ca>
2019-04-08 13:05:15 -0700

    Load texts from a YAML file

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import App from './components/app-root/App';
 import * as serviceWorker from './serviceWorker';
 import ExternalText from './utils/external-text';
 import axios from 'axios';
+import yaml from 'js-yaml';
+
 
 const loadTexts = setTexts => {
   axios.get(
     `${process.env.PUBLIC_URL}/${process.env.REACT_APP_EXTERNAL_TEXTS}`,
     {
-      responseType: 'json'
+      responseType: 'text'
     }
   )
   .then(response => response.data)
+  .then(yaml.safeLoad)
   .then(setTexts)
   .catch(error => { console.error(error); })
   ;
