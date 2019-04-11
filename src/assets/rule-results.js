42e10e59753fdf4ac243c4372877cf87b0d8d690
Rod Glover <rglover@uvic.ca>
2019-04-11 12:59:04 -0700

    Add impacts rules stub data assets

---
+++ src/assets/rule-results.js
@@ -0,0 +1,16 @@
+export default {
+  'snow': true,
+  'hybrid': true,
+  'rain': true,
+  'future-snow': true,
+  'future-hybrid': true,
+  'future-rain': true,
+  'shm': 73.26905758240526286847019043,
+  '1a-i-hydro': false,
+  '1a-ii-infra': false,
+  '1a-iii-infra': false,
+  '1a-iv-infra': false,
+  '1a-v-bio': true,
+  '1a-vi-ag': false,
+  '1b-i-hydro': true
+}
