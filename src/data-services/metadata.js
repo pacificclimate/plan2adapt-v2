c47494c9699993dbc0eb0e7c54d89413b09e25b0
Rod Glover <rglover@uvic.ca>
2019-07-03 15:21:50 -0700

    Add metadata data service

---
+++ src/data-services/metadata.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import urljoin from 'url-join';
+import flow from 'lodash/fp/flow';
+import map from 'lodash/fp/map';
+import flatten from 'lodash/fp/flatten';
+export const mapWithKey = map.convert({ cap: false });
+
+
+const getYear = timestamp => timestamp.substr(0, 4);
+
+
+export const standardize = flow(
+  // Map dict to array of array of standardized items. Each standardized item
+  // has a unique combination of values for unique_id, variable_id, and 
+  // variable_name, unrolled from the dicts in the original data,
+  // along with all the other properties that came with the dict.
+  // Properties start_date and end_date are truncated to year only.
+  mapWithKey(({ variables, start_date, end_date, ...rest }, unique_id) =>
+    mapWithKey((variable_name, variable_id) => ({
+      unique_id,
+      variable_id,
+      variable_name,
+      start_date: getYear(start_date),
+      end_date: getYear(end_date),
+      ...rest
+    }))(variables)
+  ),
+  
+  // And flatten the nested arrays.
+  flatten,
+);
+
+
+export function getMetadata() {
+  console.log('getMetadata()')
+  return axios.get(
+    urljoin(process.env.REACT_APP_CE_BACKEND_URL, 'multimeta'),
+    {
+      params: {
+        ensemble_name: process.env.REACT_APP_ENSEMBLE_NAME,
+      },
+      transformResponse: [JSON.parse, standardize],
+    },
+  )
+}
---
+++ src/data-services/metadata.test.js
@@ -0,0 +1,66 @@
+import { normalize } from './metadata';
+import sortBy from 'lodash/fp/sortBy';
+
+describe('normalize', () => {
+  const rest = i => ({
+    x: `x${i}`,
+    y: `y${i}`,
+  });
+
+  const year = (i, j) => `2${i}0${j}`;
+  const years = i => ({
+    start_date: year(i, 1),
+    end_date: year(i, 2),
+  });
+  const timestamp = (i, j) => `${year(i,j)}-foobar`;
+  const timestamps = i => ({
+    start_date: timestamp(i, 1),
+    end_date: timestamp(i, 2),
+  });
+
+  const data = {
+    uid1: {
+      ...rest(1),
+      ...timestamps(1),
+      variables: {
+        vid1: 'vname1',
+      }
+    },
+    uid2: {
+      ...rest(2),
+      ...timestamps(2),
+      variables: {
+        vid1: 'vname1',
+        vid2: 'vname2',
+      }
+    },
+  };
+
+  const normalized = [
+    {
+      unique_id: 'uid1',
+      ...rest(1),
+      ...years(1),
+      variable_id: 'vid1',
+      variable_name: 'vname1',
+    },
+    {
+      unique_id: 'uid2',
+      ...rest(2),
+      ...years(2),
+      variable_id: 'vid1',
+      variable_name: 'vname1',
+    },
+    {
+      unique_id: 'uid2',
+      ...rest(2),
+      ...years(2),
+      variable_id: 'vid2',
+      variable_name: 'vname2',
+    },
+  ];
+
+  it('works', () => {
+    expect(sortBy('unique_id')(normalize(data))).toEqual(normalized);
+  });
+});
