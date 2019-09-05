f822d4d728fafd943c1986f3b338de088487209f
Rod Glover <rglover@uvic.ca>
2019-09-04 17:10:07 -0700

    Revise TimePeriodSelector to use actual dates

--- src/components/selectors/TimePeriodSelector/TimePeriodSelector.js
+++ src/components/selectors/TimePeriodSelector/TimePeriodSelector.js
@@ -3,6 +3,14 @@ import React from 'react';
 import { SimpleConstraintGroupingSelector } from 'pcic-react-components';
 
 
+const getOptionRepresentative = ({ start_date, end_date }) =>
+  ({ start_date, end_date });
+
+const getOptionLabel = ({ value: { representative: { start_date, end_date } } }) => {
+  const decade = Math.floor((Number(start_date) + Number(end_date)) / 20 ) * 10;
+  return `${decade}s (${start_date}–${end_date})`;
+};
+
 export default class TimePeriodSelector extends React.Component {
   static propTypes = {
     bases: PropTypes.array.isRequired,
@@ -16,23 +24,11 @@ export default class TimePeriodSelector extends React.Component {
     onChange: PropTypes.func,
   };
 
-  static getOptionRepresentative = ({ start_date, end_date }) => {
-    const decade = Math.floor((Number(start_date) + Number(end_date)) / 20 ) * 10;
-    return {
-      decade,
-      start_date: decade - 10,
-      end_date: decade + 20,
-    };
-  };
-
-  static getOptionLabel = ({ value: { representative: { decade, start_date, end_date } } }) =>
-    `${decade}s (${start_date}–${end_date})`;
-
   render() {
     return (
       <SimpleConstraintGroupingSelector
-        getOptionRepresentative={TimePeriodSelector.getOptionRepresentative}
-        getOptionLabel={TimePeriodSelector.getOptionLabel}
+        getOptionRepresentative={getOptionRepresentative}
+        getOptionLabel={getOptionLabel}
         {...this.props}
       />
     );
