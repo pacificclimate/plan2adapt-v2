b14514bbbaa84e50534e50456ae543fe6200f22f
Rod Glover <rglover@uvic.ca>
2019-09-05 08:41:07 -0700

    Add CanadaBaseMap

---
+++ src/components/maps/map-utils.js
@@ -0,0 +1,11 @@
+var generateResolutions = function (maxRes, count) {
+  var result = new Array(count);
+  for (var i = 0; i < result.length; i++) {
+    result[i] = maxRes / Math.pow(2, i);
+  }
+  return result;
+};
+
+export {
+  generateResolutions,
+};
