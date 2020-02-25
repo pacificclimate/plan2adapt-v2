1122c8a1f760cc51fe725a3558aea4baec2526d9
Rod Glover <rglover@uvic.ca>
2020-02-25 14:03:18 -0800

    Update RegionSelector to fetch content from server

--- src/components/selectors/RegionSelector/RegionSelector.js
+++ src/components/selectors/RegionSelector/RegionSelector.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Select from 'react-select';
-import regions from '../../../assets/regions';
+import { fetchRegions } from '../../../data-services/regions';
+import flow from 'lodash/fp/flow';
+import map from 'lodash/fp/map';
+import groupBy from 'lodash/fp/groupBy';
+import { mapWithKey } from 'pcic-react-components/dist/utils/fp';
 
 
 export default class RegionSelector extends React.Component {
@@ -10,14 +14,44 @@ export default class RegionSelector extends React.Component {
     onChange: PropTypes.func,
   };
 
+
+  state = {
+    regions: null,
+  };
+
+  componentDidMount() {
+    this._asyncRequest = fetchRegions().then(
+      data => {
+        this._asyncRequest = null;
+        const regions = flow(
+          groupBy(feature => feature.properties.group),
+          mapWithKey((features, group) => ({
+            label: group,
+            options: map(
+              feature => ({
+                label: feature.properties.english_na,
+                // 'bc-regions-polygon.1' is the whole province; we don't
+                // want a polygon for that.
+                value: feature.id === 'bc-regions-polygon.1' ? null : feature,
+              })
+            )(features)
+          })),
+        )(data.features);
+        this.setState({ regions });
+      }
+    )
+  }
+
   render() {
     return (
       <Select
         isSearchable
-        options={regions}
+        isLoading={this.state.regions === null}
+        // loadingMessage={'Loading...'}
+        options={this.state.regions || []}
         value={this.props.value}
         onChange={this.props.onChange}
       />
     );
   }
-}
+};
