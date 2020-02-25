40833ca591d79d838b43779b94a3441bbf29c849
Rod Glover <rglover@uvic.ca>
2020-02-25 14:03:45 -0800

    Add regions data service; fake data for now (CORS issue)

---
+++ src/data-services/regions.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import tap from 'lodash/fp/tap';
+import regions from '../assets/regions';
+
+
+export function fetchRegions() {
+  // Fetch GeoJSON describing BC regions.
+  return Promise.resolve(regions);
+  console.log('### fetchRegions')
+  return axios.get(
+    process.env.REACT_APP_REGIONS_SERVICE_URL,
+    {
+      params: {
+        version: '1.0.0',
+        service: 'WFS',
+        request: 'GetFeature',
+        typeName: 'bc_regions:bc-regions-polygon',
+        maxFeatures: 100,
+        outputFormat: 'application/json',
+      }
+    }
+  )
+  .then(tap(response => {console.log('### fetchRegions: done')}))
+  .then(response => response.data)
+}
