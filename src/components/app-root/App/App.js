cedf69460cbb8fd8095110d0bd79ae82042d05bb
Rod Glover <rglover@uvic.ca>
2020-03-21 12:10:23 -0700

    Add default to TimePeriodSelector

--- src/components/app-root/App/App.js
+++ src/components/app-root/App/App.js
@@ -38,6 +38,8 @@ const baselineTimePeriod = {
 };
 
 export default class App extends Component {
+  static contextType = T.contextType;
+
   state = {
     metadata: null,
     region: undefined,
@@ -63,249 +65,245 @@ export default class App extends Component {
     // since extraction means introducing extra machinery for state-setting
     // callbacks, etc.
 
-    // TODO: Replace with About tab
-    console.log(`### Version: ${process.env.REACT_APP_VERSION}`);
-
-    if (this.state.metadata === null) {
-      console.log('Loading metadata...')
+    const texts = this.context;
+    if (!texts || this.state.metadata === null) {
       // TODO: Replace with spinner or something
-      return (<h1>Loading metadata...</h1>);
+      console.log('Loading ...')
+      return (<h1>Loading ...</h1>);
     }
-    console.log('Metadata loaded')
+    console.log('Loaded')
     const futureTimePeriod =
       get('futureTimePeriod.value.representative', this.state) || {};
     const region = get('region.label', this.state) || '';
     return (
-      // We introduce a consumer for external texts context so we can use
-      // T.get easily (it needs the context (`texts`) as an argument).
-      <ExternalTextContext.Consumer>
-        {texts => (
-          <Container fluid>
-            <AppHeader/>
+      <Container fluid>
+        <AppHeader/>
 
-            <Row>
-              <Col xl={2} lg={12} md={12}>
-                {/* TODO: Extract this as a separate component? */}
-                <div className='MainSelectors'>
-                  <Row>
-                    <Col>
-                      <T path='mainSelectors.prologue'/>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col xl={12} lg={'auto'} md={'auto'} className='pr-0'>
-                      <T path='mainSelectors.regionPrefix'/>
-                    </Col>
-                    <Col xl={12} lg={3} md={6}>
-                      <RegionSelector
-                        value={this.state.region}
-                        onChange={this.handleChangeRegion}
-                      />
-                    </Col>
-                    <Col xl={12} lg={'auto'} md={'auto'} className='pr-0'>
-                      <T path='mainSelectors.periodPrefix'/>
-                    </Col>
-                    <Col xl={12} lg={3} md={4}>
-                      <TimePeriodSelector
-                        bases={filter(m => +m.start_date >= 2010)(this.state.metadata)}
-                        value={this.state.futureTimePeriod}
-                        onChange={this.handleChangeTimePeriod}
-                        debug
-                      />
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
+        <Row>
+          <Col xl={2} lg={12} md={12}>
+            {/* TODO: Extract this as a separate component? */}
+            <div className='MainSelectors'>
+              <Row>
+                <Col>
+                  <T path='mainSelectors.prologue'/>
+                </Col>
+              </Row>
+              <Row>
+                <Col xl={12} lg={'auto'} md={'auto'} className='pr-0'>
+                  <T path='mainSelectors.regionPrefix'/>
+                </Col>
+                <Col xl={12} lg={3} md={6}>
+                  <RegionSelector
+                    value={this.state.region}
+                    onChange={this.handleChangeRegion}
+                  />
+                </Col>
+                <Col xl={12} lg={'auto'} md={'auto'} className='pr-0'>
+                  <T path='mainSelectors.periodPrefix'/>
+                </Col>
+                <Col xl={12} lg={3} md={4}>
+                  <TimePeriodSelector
+                    bases={filter(m => +m.start_date >= 2010)(this.state.metadata)}
+                    value={this.state.futureTimePeriod}
+                    default={T.get(texts, 'selectors.timePeriod.default', {}, 'raw')}
+                    onChange={this.handleChangeTimePeriod}
+                    debug
+                  />
+                </Col>
+              </Row>
+            </div>
+          </Col>
 
-              <Col xl={10} lg={12} md={12}>
-                <Tabs
-                  id={'main'}
-                  // Not sure why this doesn't work. Annoying.
-                  // defaultActiveKey={T.get(texts, 'app.tabs.defaultActiveKey')}
-                  defaultActiveKey='Summary'
-                >
-                  <Tab
-                    eventKey={T.get(texts, 'summary.tab')}
-                    title={<T as='string' path='summary.tab'/>}
-                    className='pt-2'
-                  >
-                    <Summary
-                      region={get('value', this.state.region)}
-                      futureTimePeriod={futureTimePeriod}
-                      tableContents={T.get(texts, 'summary.table.contents')}
-                    />
-                    <T path='summary.notes.general' data={{
+          <Col xl={10} lg={12} md={12}>
+            <Tabs
+              id={'main'}
+              // Not sure why this doesn't work. Annoying.
+              // defaultActiveKey={T.get(texts, 'app.tabs.defaultActiveKey')}
+              defaultActiveKey='Summary'
+            >
+              <Tab
+                eventKey={T.get(texts, 'summary.tab')}
+                title={<T as='string' path='summary.tab'/>}
+                className='pt-2'
+              >
+                <Summary
+                  region={get('value', this.state.region)}
+                  futureTimePeriod={futureTimePeriod}
+                  tableContents={T.get(texts, 'summary.table.contents')}
+                />
+                <T path='summary.notes.general' data={{
+                  region: region,
+                  futureDecade: middleDecade(futureTimePeriod),
+                  baselineDecade: middleDecade(baselineTimePeriod),
+                }}/>
+                <T path='summary.notes.derivedVars'/>
+              </Tab>
+
+              <Tab
+                eventKey={T.get(texts, 'impacts.tab')}
+                title={<T as='string' path='impacts.tab'/>}
+                className='pt-2'
+              >
+                <Row>
+                  <Col lg={12}>
+                    <T path='impacts.prologue' data={{
                       region: region,
                       futureDecade: middleDecade(futureTimePeriod),
                       baselineDecade: middleDecade(baselineTimePeriod),
                     }}/>
-                    <T path='summary.notes.derivedVars'/>
-                  </Tab>
-
-                  <Tab
-                    eventKey={T.get(texts, 'impacts.tab')}
-                    title={<T as='string' path='impacts.tab'/>}
-                    className='pt-2'
-                  >
-                    <Row>
-                      <Col lg={12}>
-                        <T path='impacts.prologue' data={{
-                          region: region,
-                          futureDecade: middleDecade(futureTimePeriod),
-                          baselineDecade: middleDecade(baselineTimePeriod),
-                        }}/>
-                        <ImpactsTab
-                          rulebase={rulebase}
-                          region={get('value', this.state.region)}
-                          futureTimePeriod={futureTimePeriod}
-                        />
-                      </Col>
-                    </Row>
-                  </Tab>
-
-                  {/*
-                  <Tab mountOnEnter> prevents premature initialization of
-                  maps leading to incorrect appearance until window is resized.
-                  */}
-                  <Tab
-                    eventKey={T.get(texts, 'maps.tab')}
-                    title={<T as='string' path='maps.tab'/>}
-                    className='pt-2'
-                    mountOnEnter
-                  >
-                    <Row>
-                      <Col xs={'auto'} className='pr-0'>
-                        <T path='components.variablePrefix'/>
-                      </Col>
-                      <Col sm={4} xs={6}>
-                        <VariableSelector
-                          bases={this.state.metadata}
-                          value={this.state.variable}
-                          onChange={this.handleChangeVariable}
-                        />
-                      </Col>
-                      <Col xs={'auto'} className='pr-0'>
-                        <T path='mainSelectors.seasonPrefix'/>
-                      </Col>
-                      <Col lg={2} sm={4} xs={6}>
-                        <SeasonSelector
-                          value={this.state.season}
-                          onChange={this.handleChangeSeason}
-                        />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12}>
-                        <T path='maps.title' data={{
-                          season: get('label', this.state.season),
-                          variable: get('label', this.state.variable),
-                          region: get('label', this.state.region),
-                        }}/>
-                      </Col>
-                    </Row>
-                    <TwoDataMaps
+                    <ImpactsTab
+                      rulebase={rulebase}
                       region={get('value', this.state.region)}
-                      historicalTimePeriod={{
-                        start_date: 1961,
-                        end_date: 1990,
-                      }}
                       futureTimePeriod={futureTimePeriod}
-                      season={get('value', this.state.season)}
-                      variable={get('value', this.state.variable)}
-                      metadata={this.state.metadata}
                     />
-                  </Tab>
+                  </Col>
+                </Row>
+              </Tab>
 
-                  <Tab
-                    eventKey={T.get(texts, 'graph.tab')}
-                    title={<T as='string' path='graph.tab'/>}
-                    className='pt-2'
-                  >
-                    <Row>
-                      <Col xs={'auto'} className='pr-0'>
-                        <T path='components.variablePrefix'/>
-                      </Col>
-                      <Col sm={4} xs={6}>
-                        <VariableSelector
-                          bases={this.state.metadata}
-                          value={this.state.variable}
-                          onChange={this.handleChangeVariable}
-                        />
-                      </Col>
-                      <Col xs={'auto'} className='pr-0'>
-                        <T path='mainSelectors.seasonPrefix'/>
-                      </Col>
-                      <Col lg={2}  sm={4} xs={6}>
-                        <SeasonSelector
-                          value={this.state.season}
-                          onChange={this.handleChangeSeason}
-                        />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg={12}>
-                        <T path='graph.title' data={{
-                          season: get('label', this.state.season),
-                          variable: get('label', this.state.variable),
-                          region: get('label', this.state.region),
-                        }}/>
-                      </Col>
-                      <Col lg={6}>
-                        <ChangeOverTimeGraph
-                          {...this.state}
-                        />
-                      </Col>
-                    </Row>
-                  </Tab>
+              {/*
+              <Tab mountOnEnter> prevents premature initialization of
+              maps leading to incorrect appearance until window is resized.
+              */}
+              <Tab
+                eventKey={T.get(texts, 'maps.tab')}
+                title={<T as='string' path='maps.tab'/>}
+                className='pt-2'
+                mountOnEnter
+              >
+                <Row>
+                  <Col xs={'auto'} className='pr-0'>
+                    <T path='components.variablePrefix'/>
+                  </Col>
+                  <Col sm={4} xs={6}>
+                    <VariableSelector
+                      bases={this.state.metadata}
+                      value={this.state.variable}
+                      // default={T.get(texts, 'selectors.variable.default', {}, 'raw')}
+                      default={'pr'}
+                      onChange={this.handleChangeVariable}
+                    />
+                  </Col>
+                  <Col xs={'auto'} className='pr-0'>
+                    <T path='mainSelectors.seasonPrefix'/>
+                  </Col>
+                  <Col lg={2} sm={4} xs={6}>
+                    <SeasonSelector
+                      value={this.state.season}
+                      onChange={this.handleChangeSeason}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg={12}>
+                    <T path='maps.title' data={{
+                      season: get('label', this.state.season),
+                      variable: get('label', this.state.variable),
+                      region: get('label', this.state.region),
+                    }}/>
+                  </Col>
+                </Row>
+                <TwoDataMaps
+                  region={get('value', this.state.region)}
+                  historicalTimePeriod={{
+                    start_date: 1961,
+                    end_date: 1990,
+                  }}
+                  futureTimePeriod={futureTimePeriod}
+                  season={get('value', this.state.season)}
+                  variable={get('value', this.state.variable)}
+                  metadata={this.state.metadata}
+                />
+              </Tab>
 
-                  <Tab
-                    eventKey={T.get(texts, 'notes.tab')}
-                    title={<T as='string' path='notes.tab'/>}
-                    className='pt-2'
-                  >
-                    <T path='notes.content'/>
-                  </Tab>
+              <Tab
+                eventKey={T.get(texts, 'graph.tab')}
+                title={<T as='string' path='graph.tab'/>}
+                className='pt-2'
+              >
+                <Row>
+                  <Col xs={'auto'} className='pr-0'>
+                    <T path='components.variablePrefix'/>
+                  </Col>
+                  <Col sm={4} xs={6}>
+                    <VariableSelector
+                      bases={this.state.metadata}
+                      value={this.state.variable}
+                      // default={T.get(texts, 'selectors.variable.default', {}, 'raw')}
+                      onChange={this.handleChangeVariable}
+                    />
+                  </Col>
+                  <Col xs={'auto'} className='pr-0'>
+                    <T path='mainSelectors.seasonPrefix'/>
+                  </Col>
+                  <Col lg={2}  sm={4} xs={6}>
+                    <SeasonSelector
+                      value={this.state.season}
+                      onChange={this.handleChangeSeason}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg={12}>
+                    <T path='graph.title' data={{
+                      season: get('label', this.state.season),
+                      variable: get('label', this.state.variable),
+                      region: get('label', this.state.region),
+                    }}/>
+                  </Col>
+                  <Col lg={6}>
+                    <ChangeOverTimeGraph
+                      {...this.state}
+                    />
+                  </Col>
+                </Row>
+              </Tab>
 
-                  <Tab
-                    eventKey={T.get(texts, 'references.tab')}
-                    title={<T as='string' path='references.tab'/>}
-                    className='pt-2'
-                  >
-                    <T path='references.content'/>
-                  </Tab>
+              <Tab
+                eventKey={T.get(texts, 'notes.tab')}
+                title={<T as='string' path='notes.tab'/>}
+                className='pt-2'
+              >
+                <T path='notes.content'/>
+              </Tab>
 
-                  <Tab
-                    eventKey={T.get(texts, 'about.tab')}
-                    title={<T as='string' path='about.tab'/>}
-                    className='pt-2'
-                  >
-                    <Tabs id={'about'} defaultActiveKey={'Plan2Adapt'}>
-                      {
-                        map(
-                          tab => (
-                            <Tab
-                              eventKey={tab.tab}
-                              title={tab.tab}
-                            >
-                              <T.Markdown source={tab.title}/>
-                              <Cards items={tab.cards}/>
-                            </Tab>
-                          )
-                        )(T.get(
-                            texts,
-                            'about.tabs',
-                            {version: process.env.REACT_APP_VERSION}
-                          )
-                        )
-                      }
-                    </Tabs>
-                  </Tab>
+              <Tab
+                eventKey={T.get(texts, 'references.tab')}
+                title={<T as='string' path='references.tab'/>}
+                className='pt-2'
+              >
+                <T path='references.content'/>
+              </Tab>
+
+              <Tab
+                eventKey={T.get(texts, 'about.tab')}
+                title={<T as='string' path='about.tab'/>}
+                className='pt-2'
+              >
+                <Tabs id={'about'} defaultActiveKey={'Plan2Adapt'}>
+                  {
+                    map(
+                      tab => (
+                        <Tab
+                          eventKey={tab.tab}
+                          title={tab.tab}
+                        >
+                          <T.Markdown source={tab.title}/>
+                          <Cards items={tab.cards}/>
+                        </Tab>
+                      )
+                    )(T.get(
+                        texts,
+                        'about.tabs',
+                        {version: process.env.REACT_APP_VERSION}
+                      )
+                    )
+                  }
                 </Tabs>
-              </Col>
-            </Row>
-          </Container>
-        )}
-      </ExternalTextContext.Consumer>
-    );
+              </Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
--- src/components/selectors/TimePeriodSelector/TimePeriodSelector.js
+++ src/components/selectors/TimePeriodSelector/TimePeriodSelector.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import curry from 'lodash/fp/curry';
+import find from 'lodash/fp/find';
 import { SimpleConstraintGroupingSelector } from 'pcic-react-components';
 import { middleDecade } from '../../../utils/time-periods';
 
@@ -7,10 +9,20 @@ import { middleDecade } from '../../../utils/time-periods';
 const getOptionRepresentative = ({ start_date, end_date }) =>
   ({ start_date, end_date });
 
-const getOptionLabel = ({ value: { representative: { start_date, end_date } } }) => {
+const getOptionLabel = (
+  { value: { representative: { start_date, end_date } } }
+) => {
   const decade = middleDecade({ start_date, end_date });
   return `${decade}s (${start_date}–${end_date})`;
 };
+
+const replaceInvalidValue = curry(
+  (decade, options, value) => {
+    return find(
+      option => middleDecade(option.value.representative) === decade
+    )(options);
+  }
+);
 
 export default class TimePeriodSelector extends React.Component {
   static propTypes = {
@@ -21,6 +33,9 @@ export default class TimePeriodSelector extends React.Component {
       // Any option that does not have a context that matches this value
       // is disabled. Replaces prop getOptionIsDisabled' in `GroupingSelector`.
 
+    default: PropTypes.number,
+    // Default value; specified by numerical decade (e.g., 2020)
+
     value: PropTypes.object,
     onChange: PropTypes.func,
   };
@@ -30,6 +45,7 @@ export default class TimePeriodSelector extends React.Component {
       <SimpleConstraintGroupingSelector
         getOptionRepresentative={getOptionRepresentative}
         getOptionLabel={getOptionLabel}
+        replaceInvalidValue={replaceInvalidValue(this.props.default)}
         {...this.props}
       />
     );
