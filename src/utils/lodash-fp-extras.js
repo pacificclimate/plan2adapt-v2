751478f6f71060ab9e6c61d9e1ea6c6bca0a4697
Rod Glover <rglover@uvic.ca>
2020-06-09 14:17:37 -0700

    Add two functions to lodash-fp-extras

--- src/utils/lodash-fp-extras.js
+++ src/utils/lodash-fp-extras.js
@@ -2,15 +2,31 @@ import fromPairs from 'lodash/fp/fromPairs';
 import flatten from 'lodash/fp/flatten';
 import isArray from 'lodash/fp/isArray';
 import flow from 'lodash/fp/flow';
-import map from 'lodash/fp/map';
 import convert from 'lodash/fp/convert';
 import placeholder from 'lodash/fp/placeholder';
+import isUndefined from 'lodash/fp/isUndefined';
+import curry from 'lodash/fp/curry';
+import map from 'lodash/fp/map';
+import get from 'lodash/fp/get';
+import every from 'lodash/fp/every';
 
 import { concatAll as stdConcatAll } from './lodash-extras';
 
 
 export const concatAll = convert('concatAll', stdConcatAll);
 concatAll.placeholder = placeholder;
+
+
+export const isDefined = v => !isUndefined(v);
+
+
+export const allDefined = curry((paths, object) => {
+  // Returns a boolean indicating whether every path in `paths` accesses
+  // a defined item in object.
+  const items = map(path => get(path, object))(paths);
+  return every(isDefined, items);
+});
+
 
 
 export const fromPairsMulti = pairs => {
