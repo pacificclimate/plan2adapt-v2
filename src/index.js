fb3298611d7414ee9d72cba161c96e7536ea134d
Rod Glover <rglover@uvic.ca>
2020-06-17 11:10:39 -0700

    Extract lethargic scrolling to utils

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Lethargy } from 'lethargy';
-import L from 'leaflet';
 import './index.css';
 import App from './components/app-root/AppWrapper';
 import * as serviceWorker from './serviceWorker';
 import ExternalText from './temporary/external-text';
 import { makeYamlLoader } from './utils/external-text';
+import { setLethargicScrolling } from './utils/leaflet-extensions';
 
-
-// Stop inertial scrolling from interfering with scroll wheel zooming.
-// See https://github.com/Leaflet/Leaflet/issues/4410#issuecomment-340905236
-const lethargy = new Lethargy(7, 50, 0.05);
-const isInertialScroll = (e) => lethargy.check(e) === false;
-L.Map.ScrollWheelZoom.prototype._onWheelScroll = function (e) {
-  L.DomEvent.stop(e);
-  if (isInertialScroll(e)) {
-    return;
-  }
-
-  this._delta += L.DomEvent.getWheelDelta(e);
-  this._lastMousePos = this._map.mouseEventToContainerPoint(e);
-  this._performZoom();
-}
+setLethargicScrolling(7, 50, 0.05);
 
 console.log('index.js: App', App)
 console.log('index.js: ExternalText', ExternalText)
---
+++ src/utils/leaflet-extensions.js
@@ -0,0 +1,21 @@
+import { Lethargy } from 'lethargy';
+import L from 'leaflet';
+
+
+// Stop inertial scrolling from interfering with scroll wheel zooming.
+// See https://github.com/Leaflet/Leaflet/issues/4410#issuecomment-340905236
+export const setLethargicScrolling = (...args) => {
+  const lethargy = new Lethargy(...args);
+  const isInertialScroll = (e) => lethargy.check(e) === false;
+  L.Map.ScrollWheelZoom.prototype._onWheelScroll = function (e) {
+    L.DomEvent.stop(e);
+    if (isInertialScroll(e)) {
+      return;
+    }
+
+    this._delta += L.DomEvent.getWheelDelta(e);
+    this._lastMousePos = this._map.mouseEventToContainerPoint(e);
+    this._performZoom();
+  }
+}
+
