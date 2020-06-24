6246ac532ea04637d5855f7d462738962ce04cee
Rod Glover <rglover@uvic.ca>
2020-06-24 10:58:21 -0700

    Remove unnecessary logging from ErrorBoundary

--- src/components/misc/ErrorBoundary.js
+++ src/components/misc/ErrorBoundary.js
@@ -10,9 +10,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-let errorCount = 0;
-
-
 export default class ErrorBoundary extends React.Component {
   static propTypes = {
     fallback: PropTypes.any,
@@ -20,14 +17,14 @@ export default class ErrorBoundary extends React.Component {
 
   static defaultProps = {
     fallback: (error, errorInfo) => {
-      console.log('### fallback: error', error)
       return (
         <div>
           <h1>Oops, something went wrong.</h1>
           <p>If you see this page, please report it to the proper
             authorities.</p>
           <p>Here's some information on the problem:</p>
-          <p>{error.error.toString()}</p>
+          <pre>{error.error.toString()}</pre>
+          <pre>{error.errorInfo.componentStack}</pre>
         </div>
       )
     },
@@ -39,8 +36,6 @@ export default class ErrorBoundary extends React.Component {
   };
 
   componentDidCatch(error, errorInfo) {
-    errorCount += 1;
-    console.log('### ErrorBoundary.componentDidCatch', errorCount)
     this.setState({ error, errorInfo });
     // You can also log the error to an error reporting service
     console.error(error, errorInfo);
@@ -48,12 +43,10 @@ export default class ErrorBoundary extends React.Component {
 
   render() {
     if (this.state.error !== null) {
-      console.log('### ErrorBoundary.render: fallback', errorCount)
       const Fallback = this.props.fallback;
       return <Fallback {...this.state} />;
     }
     // No error: Just render children.
-    console.log('### ErrorBoundary.render: children', errorCount)
     return this.props.children;
   }
 }
