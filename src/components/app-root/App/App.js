a0db2612f5503cc14d94ea5b202bfd311e4c2ebe
Rod Glover <rglover@uvic.ca>
2020-06-25 09:37:48 -0700

    Wrap selectors in ErrorBoundary

--- src/components/app-root/App/App.js
+++ src/components/app-root/App/App.js
@@ -164,11 +164,13 @@ export default class App extends Component {
                       <T path='selectors.region.prefix'/>
                     </Col>
                     <Col xl={12} lg={3} md={6}>
-                      <RegionSelector
-                        default={this.getConfig('selectors.region.default')}
-                        value={this.state.regionOpt}
-                        onChange={this.handleChangeRegion}
-                      />
+                      <ErrorBoundary>
+                        <RegionSelector
+                          default={this.getConfig('selectors.region.default')}
+                          value={this.state.regionOpt}
+                          onChange={this.handleChangeRegion}
+                        />
+                      </ErrorBoundary>
                     </Col>
                   </React.Fragment>
                 }
@@ -179,13 +181,15 @@ export default class App extends Component {
                       <T path='selectors.timePeriod.prefix'/>
                     </Col>
                     <Col xl={12} lg={3} md={4}>
-                      <TimePeriodSelector
-                        bases={filter(m => +m.start_date >= 2010)(this.state.metadata)}
-                        value={this.state.futureTimePeriodOpt}
-                        default={this.getConfig('selectors.timePeriod.default')}
-                        onChange={this.handleChangeTimePeriod}
-                        debug
-                      />
+                      <ErrorBoundary>
+                          <TimePeriodSelector
+                          bases={filter(m => +m.start_date >= 2010)(this.state.metadata)}
+                          value={this.state.futureTimePeriodOpt}
+                          default={this.getConfig('selectors.timePeriod.default')}
+                          onChange={this.handleChangeTimePeriod}
+                          debug
+                        />
+                      </ErrorBoundary>
                     </Col>
                   </React.Fragment>
                 }
@@ -196,13 +200,15 @@ export default class App extends Component {
                       <T path='selectors.variable.prefix'/>
                     </Col>
                     <Col xl={12} lg={3} md={4}>
-                      <VariableSelector
-                        bases={this.state.metadata}
-                        value={this.state.variableOpt}
-                        default={this.getConfig('selectors.variable.default')}
-                        onChange={this.handleChangeVariable}
-                        getOptionLabel={getVariableOptionLabel}
-                      />
+                      <ErrorBoundary>
+                          <VariableSelector
+                          bases={this.state.metadata}
+                          value={this.state.variableOpt}
+                          default={this.getConfig('selectors.variable.default')}
+                          onChange={this.handleChangeVariable}
+                          getOptionLabel={getVariableOptionLabel}
+                        />
+                      </ErrorBoundary>
                     </Col>
                   </React.Fragment>
                 }
@@ -213,11 +219,13 @@ export default class App extends Component {
                       <T path='selectors.season.prefix'/>
                     </Col>
                     <Col xl={12} lg={3} md={4}>
-                      <SeasonSelector
+                      <ErrorBoundary>
+                          <SeasonSelector
                           value={this.state.seasonOpt}
                           default={this.getConfig('selectors.season.default')}
                           onChange={this.handleChangeSeason}
-                      />
+                        />
+                      </ErrorBoundary>
                     </Col>
                     <Col xl={12} lg={'auto'} md={'auto'} className='pr-0'>
                       <T path='selectors.season.postfix'/>
