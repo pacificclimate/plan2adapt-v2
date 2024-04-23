0d277401f6e70a483f085c733d49e50436af49b9
Quintin <quintins@uvic.ca>
2024-04-23 10:06:52 -0700

    Get group from region properties

--- src/utils/regions.js
+++ src/utils/regions.js
@@ -1,6 +1,9 @@
-export const regionId = (region, group) => {
+export const regionId = (region) => {
+
+
   // Map frontend region specifier to region id used by the backend.
   const name = region.properties.english_na;
+  const group = region.properties.group
   switch (name) {
     case 'British Columbia': return 'bc';
     case 'Mount Waddington': return 'mt_waddington';
