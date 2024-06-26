93141c2adf897e114ad19c3b9107bbe5946e46f9
Quintin <quintins@uvic.ca>
2024-06-26 13:16:03 -0700

    Refactor StaticControl to functional component

--- src/components/maps/StaticControl/StaticControl.js
+++ src/components/maps/StaticControl/StaticControl.js
@@ -1,41 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
 import './StaticControl.css';
 
-class StaticControl extends React.Component {
-  createLeafletElement(props) {
-    const leafletElement = L.control({ position: props.position });
-
-    leafletElement.onAdd = map => {
-      this.container = L.DomUtil.create(
-        'div',
-        'StaticControl leaflet-control'
-      );
-      Object.assign(this.container.style, props.style);
-      this.root = createRoot(this.container);
-      this.root.render(props.children);
-      return this.container;
-    };
-
-    return leafletElement;
-  }
-
-  updateLeafletElement(prevProps, toProps) {
-    if (prevProps.children !== toProps.children) {
-      this.root.render(toProps.children);
-    }
-  }
-
-  render() {
-    return null;
-  }
-}
-
-const StaticControlWithMap = (props) => {
+const StaticControl = ({ position, style, children }) => {
   const map = useMap();
-  return <StaticControl {...props} map={map} />;
+
+  useEffect(() => {
+    const container = L.DomUtil.create('div', 'StaticControl leaflet-control');
+    Object.assign(container.style, style);
+    const root = createRoot(container);
+    root.render(children);
+
+    const control = L.control({ position });
+    control.onAdd = () => container;
+
+    control.addTo(map);
+
+    return () => {
+      control.remove();
+      root.unmount();
+    };
+  }, [map, position, style, children]);
+
+  return null;
 };
 
-export default StaticControlWithMap;
+export default StaticControl;
