513764b1f292d964352be7d56a35952b0b6b39ab
Quintin <quintins@uvic.ca>
2024-08-29 15:46:24 -0700

    Set climate layer opacity

--- src/components/maps/ClimateLayer/ClimateLayer.js
+++ src/components/maps/ClimateLayer/ClimateLayer.js
@@ -78,7 +78,7 @@ export default class ClimateLayer extends React.Component {
         logscale={getWmsLogscale(variableConfig, variableId)}
         noWrap={true}
         numcolorbands={wmsNumcolorbands}
-        opacity={0.0}
+        opacity={0.6}
         // srs={"EPSG:3005"}
         transparent={true}
         version={'1.1.1'}
